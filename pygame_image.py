028e7b45aedf65f8de1e4ceec0d3350cb9524538
manato <c0c2403167@edu.teu.ac.jp>
2025-11-18 17:46:00 +0900

    練習5:こうかとん飛ぶ

--- pygame_image.py
+++ pygame_image.py
@@ -16,7 +16,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr # 練習5
+        screen.blit(bg_img, [-x, 0]) # 練習5
         screen.blit(kk_img, [300, 200]) # 練習4
         pg.display.update()
         tmr += 1        

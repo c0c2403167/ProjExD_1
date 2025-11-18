3221e4edbdc4244b2a42eed40c2b308ca5a7d2d0
manato <c0c2403167@edu.teu.ac.jp>
2025-11-18 17:43:03 +0900

    練習3:こうかとん画像

--- pygame_image.py
+++ pygame_image.py
@@ -10,33 +10,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_img,True,False)
-    kk_img = pg.transform.flip(pg.image.load("fig/3.png"),True,False)
-    kk_rct=kk_img.get_rect() # 練習10
-    kk_rct.center=300, 200  # 練習10
+    kk_img = pg.transform.flip(pg.image.load("fig/3.png"), True, False) # 練習3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 3200
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg_img2,[-x+1600,0])
-
-        key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
-        screen.blit(kk_img, kk_rct)
+        screen.blit(bg_img, [0, 0])
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
